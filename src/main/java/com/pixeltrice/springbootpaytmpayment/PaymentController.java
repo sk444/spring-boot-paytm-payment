53069fa74f603dd6e9e52d452d1d96d56abb4048
shubham kumar <shubhamkumar1980@gmail.com>
2020-09-06 14:56:16 +0530

    initail commit

---
+++ src/main/java/com/pixeltrice/springbootpaytmpayment/PaymentController.java
@@ -0,0 +1,99 @@
+package com.pixeltrice.springbootpaytmpayment;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.paytm.pg.merchant.PaytmChecksum;
+
+@Controller
+public class PaymentController {
+	
+	@Autowired
+	private PaytmDetailPojo paytmDetailPojo;
+	@Autowired
+	private Environment env;
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+
+	 @PostMapping(value = "/submitPaymentDetail")
+	    public ModelAndView getRedirect(@RequestParam(name = "CUST_ID") String customerId,
+	                                    @RequestParam(name = "TXN_AMOUNT") String transactionAmount,
+	                                    @RequestParam(name = "ORDER_ID") String orderId) throws Exception {
+
+	        ModelAndView modelAndView = new ModelAndView("redirect:" + paytmDetailPojo.getPaytmUrl());
+	        TreeMap<String, String> parameters = new TreeMap<>();
+	        paytmDetailPojo.getDetails().forEach((k, v) -> parameters.put(k, v));
+	        parameters.put("MOBILE_NO", env.getProperty("paytm.mobile"));
+	        parameters.put("EMAIL", env.getProperty("paytm.email"));
+	        parameters.put("ORDER_ID", orderId);
+	        parameters.put("TXN_AMOUNT", transactionAmount);
+	        parameters.put("CUST_ID", customerId);
+	        String checkSum = getCheckSum(parameters);
+	        parameters.put("CHECKSUMHASH", checkSum);
+	        modelAndView.addAllObjects(parameters);
+	        return modelAndView;
+	    }
+	 
+	 
+	 @PostMapping(value = "/pgresponse")
+	    public String getResponseRedirect(HttpServletRequest request, Model model) {
+
+	        Map<String, String[]> mapData = request.getParameterMap();
+	        TreeMap<String, String> parameters = new TreeMap<String, String>();
+	        mapData.forEach((key, val) -> parameters.put(key, val[0]));
+	        String paytmChecksum = "";
+	        if (mapData.containsKey("CHECKSUMHASH")) {
+	            paytmChecksum = mapData.get("CHECKSUMHASH")[0];
+	        }
+	        String result;
+
+	        boolean isValideChecksum = false;
+	        System.out.println("RESULT : "+parameters.toString());
+	        try {
+	            isValideChecksum = validateCheckSum(parameters, paytmChecksum);
+	            if (isValideChecksum && parameters.containsKey("RESPCODE")) {
+	                if (parameters.get("RESPCODE").equals("01")) {
+	                    result = "Payment Successful";
+	                } else {
+	                    result = "Payment Failed";
+	                }
+	            } else {
+	                result = "Checksum mismatched";
+	            }
+	        } catch (Exception e) {
+	            result = e.toString();
+	        }
+	        model.addAttribute("result",result);
+	        parameters.remove("CHECKSUMHASH");
+	        model.addAttribute("parameters",parameters);
+	        return "report";
+	    }
+
+	    private boolean validateCheckSum(TreeMap<String, String> parameters, String paytmChecksum) throws Exception {
+	        return PaytmChecksum.verifySignature(parameters,
+	                paytmDetailPojo.getMerchantKey(), paytmChecksum);
+	    }
+
+
+	private String getCheckSum(TreeMap<String, String> parameters) throws Exception {
+		return PaytmChecksum.generateSignature(parameters, paytmDetailPojo.getMerchantKey());
+	}
+	
+	
+	
+}
+
---
+++ src/main/java/com/pixeltrice/springbootpaytmpayment/SpringBootPaytmPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.springbootpaytmpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaytmPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPaytmPaymentApplication.class, args);
+	}
+
+}
